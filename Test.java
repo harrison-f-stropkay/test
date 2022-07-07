691242c21759cb1ea0ddcad3b0a4c609f50cdbf9
harrisons258 <108366703+harrisonstropkay@users.noreply.github.com>
2022-07-07 15:52:37 -0400

    S

--- Test.java
+++ Test.java
@@ -3,11 +3,7 @@ import java.util.*;
 public class Test{
 	
 	public static void main(String args[]) {
-		System.out.println("test print");
-		System.out.println("test print #2");
-		System.out.println("test print #3");
-		System.out.println("test print #4");
-		System.out.println("test print #5");
-		System.out.println("test print #6");
+		
+		//s
 	}
 }
